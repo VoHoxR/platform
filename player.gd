21a68e1bd772a1682ce5b535a57ca7fbd111a27c
VoHoxR <vo.hoxr@gmail.com>
2023-10-24 11:15:29 -0500

    Mar- I mean, Bazza bogan dies now and does a cool thingy when dying

--- player.gd
+++ player.gd
@@ -1,19 +1,24 @@
 extends CharacterBody2D
 
+var is_dying = false
 var is_jumping = false
 
 var SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var death_timer = $death_timer
 
 func _ready():
 	add_to_group("Player")
+	death_timer.connect("timeout", Callable(self, "_on_DeathTimer"))
 
 func _physics_process(delta):
+	if is_dying:
+		return
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -35,6 +40,9 @@ func _physics_process(delta):
 	move_and_slide()
 
 func update_animation(direction):
+	if is_dying:
+		return
+		
 	if is_jumping:
 		animated_sprite_2d.play("jump")
 	if direction != 0:
@@ -42,3 +50,33 @@ func update_animation(direction):
 		animated_sprite_2d.play("run")
 	else:
 		animated_sprite_2d.play("idle")
+
+
+func _on_hitbox_body_entered(body):
+	if body.is_in_group("Enemy") and body.is_alive:
+		die()
+
+func die():
+	if is_dying:
+		return
+		
+	is_dying = true
+	animated_sprite_2d.play("die")
+	await move_player_up_and_down()
+	get_tree().reload_current_scene()
+
+func move_player_up_and_down():
+	var start_position = position
+	var up_position = start_position + Vector2(0, -75)
+	var down_position = start_position + Vector2(0, 600)
+	
+	while position.y > up_position.y:
+		position.y -= 4
+		await get_tree().create_timer(0.01).timeout
+		
+	while position.y < down_position.y:
+		position.y += 4
+		await get_tree().create_timer(0.01).timeout
+		
+func on_DeathTimer_timeot():
+	get_tree().reload_current_scene()

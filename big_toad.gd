0097e81d5d6f5fbf632693a81e7c02fe7c08ea53
VoHoxR <vo.hoxr@gmail.com>
2023-10-25 11:03:56 -0500

    Added big fro- Toad, that doesn't fall off like an IDIOT

--- big_toad.gd
+++ big_toad.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 25.0
-
+var SPEED = -50.0
+var facing_right = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var is_alive = true
 @onready var animated_sprite_2d = $AnimatedSprite2D
@@ -11,10 +11,15 @@ func _ready():
 	add_to_group("Enemy")
 	
 func _physics_process(delta):
-
-	velocity.y += gravity * delta
-
-	velocity.x = -SPEED
+	
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	
+	if !$RayCast2D.is_colliding() && is_on_floor():
+		flip()
+	
+	velocity.x = SPEED
 	
 	update_animation()
 	move_and_slide()
@@ -28,3 +33,12 @@ func _on_hitbox_body_entered(body):
 		is_alive = false
 		queue_free()
 
+func flip():
+	facing_right = !facing_right
+	scale.x = abs(scale.x) * -1
+	if facing_right:
+		SPEED = abs(SPEED)
+	else:
+		SPEED = abs(SPEED) * -1
+		
+		
